20dd4473c60cc9303d140e22b9c1b29816cf12f8
justaway <eastaveracruz@gmail.com>
2018-06-25 20:54:54 +0300

    1

---
+++ src/main/java/com/BooksEntity.java
@@ -0,0 +1,72 @@
+package com;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "books", schema = "lessons")
+public class BooksEntity {
+    private int id;
+    private String title;
+    private Timestamp date;
+
+    public BooksEntity(String title, Timestamp date) {
+        this.title = title;
+        this.date = date;
+    }
+
+    public BooksEntity() {
+    }
+
+    @Id @GeneratedValue
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "title", nullable = false, length = 255)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Basic
+    @Column(name = "date", nullable = false)
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BooksEntity that = (BooksEntity) o;
+
+        if (id != that.id) return false;
+        if (title != null ? !title.equals(that.title) : that.title != null) return false;
+        if (date != null ? !date.equals(that.date) : that.date != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/com/BooksStore.java
@@ -0,0 +1,33 @@
+package com;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class BooksStore {
+    public static void main(String[] args) {
+        EntityManagerFactory bookStore = Persistence.createEntityManagerFactory("book");
+        EntityManager entityManager = bookStore.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+        entityManager.persist(new BooksEntity("hell323", timestamp));
+        transaction.commit();
+
+
+//        transaction.begin();
+//        BooksEntity booksEntity = entityManager.
+//        transaction.commit();
+        entityManager.close();
+        bookStore.close();
+
+
+
+    }
+}
